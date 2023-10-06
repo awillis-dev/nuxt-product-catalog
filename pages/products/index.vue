a1a2215d486bb3cd8dd42bb300b34616b29ed6a4
Alexis Willis <alexis.willis.dev@gmail.comm>
2023-10-06 07:31:20 -0700

    add productCard that displays product img, title, and "view details" button linking to details page

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div class="grid grid-cols-4 gap-5">
-      <div v-for="product in products" :key="product.id">
-        <NuxtLink :to="`/products/${product.id}`">{{ product.title }}</NuxtLink>
+      <div v-for="p in products" :key="p.id">
+        <ProductCard :product="p" />
       </div>
     </div>
   </div>
